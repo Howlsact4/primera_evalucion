28d6e1894aa3beb1799fe9fb6d966b33ec891af7
Act4 <rocolks@gmail.com>
2020-10-06 17:00:19 -0300

    Initial commit

---
+++ app/src/main/java/com/example/primera_evalucion/Detail_activity.java
@@ -0,0 +1,13 @@
+package com.example.primera_evalucion;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Detail_activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.detail_activity);
+    }
+}
